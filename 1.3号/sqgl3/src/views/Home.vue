eb885c3bc5ee1621e793da8cd714e55193bcb735
liuyiyi <1709192410@qq.com>
2023-01-03 20:11:26 +0800

    lyy

---
+++ 1.3号/sqgl3/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class=''></div>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from 'vue';
+
+ export default {
+setup(props, ctx) {
+
+const data = reactive({});
+return {data };
+ },
+ };
+</script>
+
+<style lang='scss'>
+
+</style>
+
+  
